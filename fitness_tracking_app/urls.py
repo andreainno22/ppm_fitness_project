76b35e1c6cb64068faaf9714772b052659098ce4
andreainno22 <andrea.innocenti12@stud.unifi.it>
2023-06-13 10:56:20 +0200

    deploy

--- fitness_tracking_app/urls.py
+++ fitness_tracking_app/urls.py
@@ -1,19 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import SignUpView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("signUp/", SignUpView.as_view(), name="signUp"),
-    path('login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_change_form/', auth_views.PasswordChangeView.as_view(), name='password_change_form'),
-    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('fitness_tracking_app/', include("django.contrib.auth.urls")),
+   # path("signUp/", SignUpView.as_view(), name="signUp"),
+    #path('login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    #path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #path('password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password_change_form/', auth_views.PasswordChangeView.as_view(), name='password_change_form'),
+    #path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('workouts_add/', views.workout_add_view, name='workouts_add'),
     path('workouts_list/workouts_selection/<int:id>', views.workout_selection_view, name='workouts_selection'),
     path('workouts_delete/', views.workout_delete_view, name='workouts_delete'),
